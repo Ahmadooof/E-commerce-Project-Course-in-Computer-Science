bc9813d711f0842fd5eedd6a2ed0a388141808a3
Dustin Payne <dp222gr@student.lnu.se>
2019-04-16 11:31:16 +0200

    removed my name from readme

--- WebShop/src/app/auth-guard.service.ts
+++ WebShop/src/app/auth-guard.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
-import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
