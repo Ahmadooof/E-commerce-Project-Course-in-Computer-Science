8e1b5bff7fd0fe2b6e500e71f7783ea6e99f1986
Dustin Payne <dp222gr@student.lnu.se>
2019-05-06 12:06:27 +0200

    added ability to read from the product in database

--- WebShop/src/app/products/product-page/product-page.component.ts
+++ WebShop/src/app/products/product-page/product-page.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ShoppingCartService } from 'src/app/shopping-cart.service';
 import { ProductService } from 'src/app/product.service';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-product-page',
@@ -16,9 +21,26 @@ export class ProductPageComponent implements OnInit {
   @Input('link') link: String;
   @Input('hasLink') hasLink: boolean;
 
-  constructor(private shoppingCartService: ShoppingCartService, private productsService: ProductService) { }
+  productID;
+  // myProduct: Product;
+  myProd$: Observable<any>;
+
+  constructor(private shoppingCartService: ShoppingCartService, private ps: ProductService, private router: Router) {
+
+    this.productID = this.router.url.substr(this.router.url.lastIndexOf('=') + 1);
+
+    console.log(this.productID);
+
+    // ps.get(this.productID).snapshotChanges().pipe(
+    //   map(a => ({ key: a.key, ...a.payload.val() } as Product))
+    //   ).subscribe(p => this.myProduct = p);
+    
+    this.myProd$ = ps.get(this.productID).valueChanges();
+
+    }
 
   ngOnInit() {
+    
   }
 
 }
