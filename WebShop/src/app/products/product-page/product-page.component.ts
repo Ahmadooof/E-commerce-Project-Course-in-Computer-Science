5c1b71a635ceece66e2e216c2d654a5fd73625e8
Andrei Neagu <neaguandrei47@gmail.com>
2019-05-06 18:58:46 +0200

    fileLink in the product page

--- WebShop/src/app/products/product-page/product-page.component.ts
+++ WebShop/src/app/products/product-page/product-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ShoppingCartService } from 'src/app/shopping-cart.service';
 import { ProductService } from 'src/app/product.service';
+import { FileLinkService} from 'src/app/file-link.service';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
@@ -24,8 +25,14 @@ export class ProductPageComponent implements OnInit {
   productID;
   // myProduct: Product;
   myProd$: Observable<any>;
+  linkObject ;
+  downloadLink: String;
 
-  constructor(private shoppingCartService: ShoppingCartService, private ps: ProductService, private router: Router) {
+
+  constructor(private shoppingCartService: ShoppingCartService,
+              private ps: ProductService, 
+              private router: Router, 
+              private ls : FileLinkService) {
 
     this.productID = this.router.url.substr(this.router.url.lastIndexOf('=') + 1);
 
@@ -35,8 +42,11 @@ export class ProductPageComponent implements OnInit {
     //   map(a => ({ key: a.key, ...a.payload.val() } as Product))
     //   ).subscribe(p => this.myProduct = p);
     
+    //this code snipped in the constructor geths the link for the additional
+    // material if it exists and puts it downloadLink: String
     this.myProd$ = ps.get(this.productID).valueChanges();
-
+    this.linkObject = ls.get(this.productID);
+    this.linkObject.snapshotChanges().subscribe(a => this.downloadLink=a.payload.val() );
     }
 
   ngOnInit() {
