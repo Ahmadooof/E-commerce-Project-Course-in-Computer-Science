5d1a3a59715d0d0ece25292cb8beaa5d92a81401
Cusatelli <einarvandevelde@gmail.com>
2019-05-15 15:45:18 +0200

    Got HTML to recognize address list...

--- WebShop/src/app/user-profile/user-profile.component.ts
+++ WebShop/src/app/user-profile/user-profile.component.ts
@@ -3,7 +3,13 @@ import { AuthService } from '../auth.service';
 import { UserService } from '../user.service';
 import { Subscription } from 'rxjs/Subscription';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable, of } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+
+import { take, combineAll } from 'rxjs/operators';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/operators/mergeAll';
 
 @Component({
   selector: 'app-user-profile',
@@ -14,6 +20,11 @@ export class UserProfileComponent implements OnInit {
   userSubscription: Subscription;
   userId: string;
   addressIsSaved: boolean;
+  address$: Observable<any>;
+  /*TEST*/items: AngularFireList<any>;
+  /*TEST*/items$: Observable<any[]>;
+  /*TEST*///public test$: Observable<any>;
+  /*TEST*///public test2$: Observable<string>;
 
   checkbox = {
     checked: false
@@ -31,11 +42,22 @@ export class UserProfileComponent implements OnInit {
 
   constructor(public authService: AuthService,
     public userService: UserService, public afAuth: AngularFireAuth,
-    public db: AngularFireDatabase) { }
+    public db: AngularFireDatabase) {
+
+    /*TEST*///this.address$ = db.list('users').valueChanges(); // Get users and email
+
+    //this.address$ = this.getFirstName();
+
+  }
 
   async ngOnInit() {
     this.userSubscription = this.authService.user$.subscribe(user =>
       this.userId = user.uid);
+
+    /*TEST*/this.items = this.db.list('users');
+    /*TEST*/this.items$ = this.items.valueChanges();
+    /*TEST*///this.test2$ = of('firstname');
+    /*TEST*///this.test$ = this.getData();
   }
 
   ngOnDestroy() {
@@ -45,7 +67,7 @@ export class UserProfileComponent implements OnInit {
   submitAddress(data) {
     this.userService.saveAddress(this.userId, data);
     this.addressIsSaved = true;
-    
+
     /*return this.db.database.ref().child('/users/' + this.userId + '/displayAddress/')
       .set(this.addressIsSaved); // Set the boolean to true.*/
   }
@@ -64,4 +86,36 @@ export class UserProfileComponent implements OnInit {
     }
   }*/
 
+  /*TEST*///getData() {
+  /*return this.test2$.pipe(
+    // switchMap retruns a different observable for each 
+    // emitted value by the source (this.test2$):
+    switchMap(test2 => {
+      // No need to use native firebase api, can be done with angular/fire
+      return this.db.list('/users/', ref =>
+      ref.orderByChild('test2')
+        .equalTo(test2)
+        .limitToFirst(1))
+          .snapshotChanges()
+          .pipe(
+            map(actions => actions.map(action => {
+            const key = action.payload.key;
+            const data = action.payload.val();
+            return {key, ...data}
+            }))
+          )
+      }));*/
 }
+
+  //getFirstName() {
+    /*this.db.list('/users/'), {
+      query: {
+        orderByChild: ' ',
+        equalTo: true
+      }
+    });*/
+    /*return this.db.object('/users/' + this.userId + '/address/')
+        .snapshotChanges().pipe();*/
+  //}
+
+//}
