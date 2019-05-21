8ad456a6d4e1d0df7eaa465da4176b82d898e9c9
Lena Tevar <ht222fd@student.lnu.se>
2019-05-21 13:53:19 +0200

    minor changes and switch for status

--- WebShop/src/app/admin/admin-orders/admin-orders.component.ts
+++ WebShop/src/app/admin/admin-orders/admin-orders.component.ts
@@ -9,8 +9,28 @@ import { OrderService } from 'src/app/order.service';
 export class AdminOrdersComponent {
 
   orders$;
+  selectedStatus = 0;
   constructor(private orderService: OrderService) { 
     this.orders$ = orderService.getOrders();
   }
 
+  statusChanged(value){
+    switch (+value) {
+      case 0:  
+        console.log("In progress");
+        break;
+      case 1:
+          console.log("Processing");
+        break;
+      case 2:
+          console.log("Completed");
+        break;    
+      default:
+        break;
+    }
+  }
+
+  updateOrder(order){
+
+  }
 }
--- WebShop/src/app/models/Order.ts
+++ WebShop/src/app/models/Order.ts
@@ -1,12 +1,18 @@
 import { ShoppingCart } from './shopping-cart';
 
 export class Order {
+    id?:string;
     datePlaced: number;
     items: any[];
     total: number;
+    status: string;
     
-    constructor( public userId: string, public shipping: any,
-         shoppingCart: ShoppingCart, public totalCost: number ){
+    constructor(public userId: string, 
+      public shipping: any,
+      public shoppingCart: ShoppingCart, 
+      public totalCost: number){
+
+        this.status = "In Progress";
         this.datePlaced = new Date().getTime();
         this.totalCost;
         this.items = shoppingCart.items.map(i => {
